7d4a0c9aee70b5bb13a848e2a26d71c414553ab7
Aleksandr Maksimov <aleks.a.maksimov@gmail.com>
2020-12-20 01:35:13 +0300

    working simple

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 int main() {
   uint32_t windowWidth = 640;
   uint32_t windowHeight = 480;
-  GLFWwindow *window = prepareWindow(windowHeight, windowHeight, "Honor");
+  GLFWwindow *window = prepareWindow(windowHeight, windowWidth, "Honor");
   loadGl();
 
   // configure gl
@@ -25,15 +25,14 @@ int main() {
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LESS);
 
-  Camera camera = Camera(glm::vec3(20, 20, 20));
-  Shader theShader("shaders/color_vertex.glsl", "shaders/color_fragment.glsl");
-  theShader.setMat4("projection", camera.GetProjectionMatrix(windowWidth, windowHeight));
+  Camera camera = Camera(glm::vec3(0, 0, -1));
+  Shader shader("shaders/simple_vertex.glsl", "shaders/simple_fragment.glsl");
 
-  float vertices[] = {     //mormal           //color
-      0.5f,  0.5f,  0.0f,  0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, // верхняя правая
-      0.5f, -0.5f,  0.0f,  0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // нижняя правая
-      -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // нижняя левая
-      -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f  // верхняя левая
+  float vertices[] = {
+      0.5f,  0.5f,  0.0f,
+      0.5f, -0.5f,  0.0f,
+      -0.5f, -0.5f, 0.0f,
+      -0.5f,  0.5f, 0.0f,
   };
 
   unsigned int indices[] = {  // помните, что мы начинаем с 0!
@@ -41,7 +40,6 @@ int main() {
       1, 2, 3  // второй треугольник
   };
 
-  //begin strange
   unsigned int VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
@@ -54,40 +52,30 @@ int main() {
 
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)0);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(3 * sizeof(float)));
-  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(3 * sizeof(float)));
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  glEnableVertexAttribArray(1);
-  glEnableVertexAttribArray(2);
+
+  // обратите внимание, что данное действие разрешено, вызов glVertexAttribPointer зарегистрировал VBO как привязанный вершинный буферный объект для вершинного атрибута, так что после мы можем спокойно отвязать
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // помните: не отвязывайте EBO, пока VАО активен, поскольку связанного объект буфера элемента хранится в VАО; сохраняйте привязку EBO.
+  //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+  // После этого вы можете отменить привязку VАО, чтобы другие вызовы VАО случайно не изменили этот VAO, но это редко происходит.
+  // Изменение других значений VAO требует вызова glBindVertexArray в любом случае, поэтому мы обычно не снимаем привязку VAO (или VBO), когда это непосредственно не требуется.
+  glBindVertexArray(0);
+
 
   while (!glfwWindowShouldClose(window)) {
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
-    if (width != windowWidth or height != windowHeight){
-      theShader.setMat4("projection", camera.GetProjectionMatrix(float(width), float(height)));
-      windowWidth = width;
-      windowHeight = height;
-    }
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    theShader.Use();
-    theShader.setMat4("view", camera.GetViewMatrix());
-    theShader.setVec3("light.direction", 1.0f, 1.0f, 1.0f);
-    theShader.setVec3("viewPos", camera.Position);
-
-    // свойства света
-    theShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
-    theShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-    theShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-
-
-    glm::mat4 model = glm::mat4(1.0f);
-    theShader.setMat4("model", model);
-    // begin strange
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
+    shader.Use();
+    glBindVertexArray(VAO); // поскольку у нас есть только один VАО, нет необходимости связывать его каждый раз, но мы сделаем это, чтобы все было немного более организованно
+    //glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
--- shaders/simple_fragment.glsl
+++ shaders/simple_fragment.glsl
@@ -2,39 +2,7 @@
 
 out vec4 FragColor;
 
-struct Light {
-    vec3 direction;
-
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-};
-
-in vec3 FragPos;
-in vec3 Normal;
-in vec3 Color;
-
-uniform vec3 viewPos;
-uniform Light light;
-
 void main()
 {
-    // ambient
-    vec3 ambient = light.ambient * Color.rgb;
-
-    // diffuse
-    vec3 norm = normalize(Normal);
-    // vec3 lightDir = normalize(light.position - FragPos);
-    vec3 lightDir = normalize(-light.direction);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * Color.rgb;
-
-    // specular
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = max(dot(viewDir, reflectDir), 0.0);
-    vec3 specular = light.specular * spec * Color.rgb;
-
-    vec3 result = ambient + diffuse + specular;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
